5ca50da591f5c00e6d9ec54300f7e7b2dd1a451d
mubonassi <115490684+mubonassi@users.noreply.github.com>
2026-08-05 12:51:23 +0000

    asdasdasdasdasd

---
+++ conteudos/cap01.py
@@ -0,0 +1,8 @@
+#Funções/Comandos/Instruções: Executam uma tarefa/um código dentro do programa
+#exemplo:
+#função() - comando/função simples
+#função(parametro) - função com parametro
+
+#Exemplo de Comando
+#print() - ele exibe um texto/informação no terminal
+print("Hello, World!")
