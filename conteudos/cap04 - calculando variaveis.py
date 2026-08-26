f0c2d79c98c74ad1d14c3edde09e7308649f5a33
mubonassi <115490684+mubonassi@users.noreply.github.com>
2026-08-26 11:40:49 +0000

    Pending changes exported from your codespace

---
+++ conteudos/cap04 - calculando variaveis.py
@@ -0,0 +1,34 @@
+#Calculando Valores/Variaveis
+
+#Calculando Números (Int ou Float)
+num1 = 10
+num2 = 20
+conta = num1+num2
+print(f"{num1} + {num2} = {conta}")
+
+#Calculando Texto (String)
+valor1 = "banana"
+valor2 = "maçã"
+palavra = valor1 + " " + valor2
+print(palavra)
+
+#Operadores
+#+ - * / ** % //
+
+#Calculando Tipos Diferentes de Variaveis
+#Dois tipos diferentes NÃO PODEM se misturar
+#...porém. Eles podem ser convertidos
+
+#Convertendo tipos de variaveis
+#int() - convertendo valor para int/inteiro
+#str() - convertendo valor para string/texto
+#float() - convertando valor para float/real
+valor1 = int("40")
+valor2 = 39
+calculo = valor1+valor2
+print(calculo)
+
+valor1 = "numero"
+valor2 = 37
+juntos = valor1 + " " + str(valor2)
+print(juntos)
---
+++ desafios/desafio01.py
@@ -0,0 +1,7 @@
+print("| QUEM É ESSE POKÉMON... Quem sou eu |")
+
+nome = "Murilo Bonassi"
+idade = 32
+cor_favorita = "Roxo/Rosa/Vermelho"
+
+print(f"Meu nome é {nome}, eu tenho {idade} anos, e minhas cores favoritas são {cor_favorita}")
