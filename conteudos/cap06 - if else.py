e6dc35b551d3d3f8593f8365baa0b06f1a2b58f7
mubonassi <mpbonassi@gmail.com>
2026-09-16 10:13:47 -0300

    cvbn

---
+++ conteudos/cap06 - if else.py
@@ -0,0 +1,26 @@
+#Estruturas de Condição - If Else
+#Permite que certos blocos de códigos sejam executados apenas quando uma condição for verdadeira
+
+#Recebendo e verificando
+numero = int(input("Digite um número: "))
+
+#if > condição > ação
+if numero == 10:
+    print("Você digitou o número 10")
+
+if numero > 5:
+    print("Você digitou um número maior que 5")
+else:
+    print("Você NÃO digitou um número maior que 5")
+
+#Sinais de Comparação -> Comparadores
+# == -> Igual a -> Valor = Valor
+# > -> Maior que -> Valor > Valor
+# < -> Menor que -> Valor < Valor
+# >= -> Maior ou igual a -> Valor >= Valor
+# <= -> Menor ou igual a -> Valor <= Valor
+# != -> Diferente de -> Valor != Valor
+
+#Diferença entre = e ==
+# = -> Atribuição -> nome = leo
+# == -> Comparação -> nome == leo?
---
+++ desafios/desafio13.py
@@ -0,0 +1,10 @@
+print("| PALAVRA MÁGICA |")
+print("-"*60)
+
+tentativa = input("Digite a sua tentativa: ")
+palavra = "mítico"
+
+if tentativa == palavra:
+    print("Você acertou! Seja bem vindo!")
+else:
+    print("NEGADO!")
